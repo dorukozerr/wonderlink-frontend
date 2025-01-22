7f74f10cb58e7110377acc63ee960686952fed72
Doruk Özer <dorukozer@protonmail.com>
2025-01-22 20:03:23 +0300

    feat: filter row responsiveness

--- src/components/filter-row.tsx
+++ src/components/filter-row.tsx
@@ -1,4 +1,4 @@
-import { Calendar as CalendarIcon } from 'lucide-react';
+import { Calendar as CalendarIcon, Filter } from 'lucide-react';
 
 import { platforms, countries } from '@/lib/constants';
 import { cn } from '@/lib/utils';
@@ -7,6 +7,13 @@ import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
   DropdownMenuCheckboxItem
 } from '@/components/ui/dropdown-menu';
@@ -22,13 +29,15 @@ export const FilterRow = () => {
 
   return (
     <div className='flex h-max w-full items-center justify-end gap-2 border-b border-border p-4'>
+      <h6 className='block font-bold md:hidden'>Filters</h6>
+      <span className='flex flex-1 md:hidden' />
       <Popover>
         <PopoverTrigger asChild>
           <Button
             id='date'
             variant={'outline'}
             className={cn(
-              'w-[300px] justify-start text-left font-normal',
+              'hidden w-[300px] justify-start text-left font-normal md:flex',
               !filters.date.from && 'text-muted-foreground'
             )}
           >
@@ -89,7 +98,7 @@ export const FilterRow = () => {
       </Popover>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant='outline' className='capitalize'>
+          <Button variant='outline' className='hidden capitalize md:flex'>
             Platform -{' '}
             {filters.platforms.length === 0 ? 'All' : filters.platforms.length}
           </Button>
@@ -116,7 +125,7 @@ export const FilterRow = () => {
       </DropdownMenu>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant='outline' className='capitalize'>
+          <Button variant='outline' className='hidden capitalize md:flex'>
             Countries -{' '}
             {filters.countries.length === 0 ? 'All' : filters.countries.length}
           </Button>
@@ -139,6 +148,147 @@ export const FilterRow = () => {
               {country}
             </DropdownMenuCheckboxItem>
           ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline' size='icon' className='flex md:hidden'>
+            <Filter />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56' align='end'>
+          <DropdownMenuLabel>Filters</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <CalendarIcon />
+                {filters.date.from ? (
+                  filters.date.to ? (
+                    <>
+                      {new Date(filters.date.from).toLocaleDateString()} -{' '}
+                      {new Date(filters.date.to).toLocaleDateString()}
+                    </>
+                  ) : (
+                    new Date(filters.date.from).toLocaleDateString()
+                  )
+                ) : (
+                  <span>Pick a date</span>
+                )}
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <Calendar
+                    initialFocus
+                    mode='range'
+                    defaultMonth={new Date()}
+                    selected={
+                      filters.date.from
+                        ? filters.date.to
+                          ? {
+                              from: new Date(filters.date.from),
+                              to: new Date(filters.date.to)
+                            }
+                          : { from: new Date(filters.date.from), to: undefined }
+                        : undefined
+                    }
+                    onSelect={(newRange) =>
+                      newRange
+                        ? newRange.from
+                          ? newRange.to
+                            ? setFilters('date', {
+                                from: newRange.from.toISOString(),
+                                to: newRange.to.toISOString()
+                              })
+                            : setFilters('date', {
+                                from: newRange.from.toISOString(),
+                                to: undefined
+                              })
+                          : setFilters('date', {
+                              from: undefined,
+                              to: undefined
+                            })
+                        : setFilters('date', {
+                            from: undefined,
+                            to: undefined
+                          })
+                    }
+                    numberOfMonths={1}
+                  />
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                Platform -{' '}
+                {filters.platforms.length === 0
+                  ? 'All'
+                  : filters.platforms.length}
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  {platforms.map((platform, index) => (
+                    <DropdownMenuCheckboxItem
+                      key={`platformFilterOption-${index}`}
+                      checked={filters.platforms.includes(platform)}
+                      className='capitalize'
+                      onClick={() =>
+                        filters.platforms.includes(platform)
+                          ? setFilters(
+                              'platforms',
+                              filters.platforms.filter((p) => p !== platform)
+                            )
+                          : setFilters('platforms', [
+                              ...filters.platforms,
+                              platform
+                            ])
+                      }
+                    >
+                      {platform}
+                    </DropdownMenuCheckboxItem>
+                  ))}
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                Countries -{' '}
+                {filters.countries.length === 0
+                  ? 'All'
+                  : filters.countries.length}
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  {countries.map((country, index) => (
+                    <DropdownMenuCheckboxItem
+                      key={`countryFilterOption-${index}`}
+                      checked={filters.countries.includes(country)}
+                      className='capitalize'
+                      onClick={() =>
+                        filters.countries.includes(country)
+                          ? setFilters(
+                              'countries',
+                              filters.countries.filter((c) => c !== country)
+                            )
+                          : setFilters('countries', [
+                              ...filters.countries,
+                              country
+                            ])
+                      }
+                    >
+                      {country}
+                    </DropdownMenuCheckboxItem>
+                  ))}
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
