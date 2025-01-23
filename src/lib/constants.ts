4f7bc07e1a2ebbb6c41a33c92a854b85f5d6a0d8
Doruk Özer <dorukozer@protonmail.com>
2025-01-23 16:54:03 +0300

    chore: view states reduced

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const themeOptions = ['dark', 'light', 'system'] as const;
 
 export const viewStates = [
-  'consecutive',
-  'country',
-  'dailyUsage',
-  'platform',
+  //  'consecutive',
+  //  'country',
+  //  'dailyUsage',
+  //  'platform',
   'retention'
 ] as const;
